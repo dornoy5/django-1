21a7244823da08c2a3cc2d1f2783b1bd2193fd13
דור <dornoy5@gmail.com>
2024-03-19 12:05:30 +0200

    login register and crud for product

--- base/models.py
+++ base/models.py
@@ -16,7 +16,8 @@ class UserProfileInfo(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
-
+    
+    
     def __str__(self):
         return self.name
 # --------------------------------------------------------------------------------------------------------------------
